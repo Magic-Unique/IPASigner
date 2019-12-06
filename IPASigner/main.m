c912b7abe8d88a7c7d47f0d07b08a8ffb30443fc
冷秋 <516563564@qq.com>
2019-12-06 22:28:10 +0800

    Release: 1.1.0

--- IPASigner/main.m
+++ IPASigner/main.m
@@ -12,7 +12,7 @@
 int main(int argc, const char * argv[]) {
 	@autoreleasepool {
 		CLCommand.mainCommand.explain = @"An signer tools for ipa file.";
-		CLCommand.mainCommand.version = @"1.0.0";
+		CLCommand.mainCommand.version = @"1.1.0";
 		CLCommand.parametersSortType = CLSortTypeByAddingQueue;
 		CLMakeSubcommand(IPASigner, __init_);
 		return [CLCommand process];
